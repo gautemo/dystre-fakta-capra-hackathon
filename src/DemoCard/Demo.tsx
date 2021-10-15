4713ab2752e436cb6ff95d976275b9a4a8f5ee4d
asmundh <aasmund.haugse@gmail.com>
2021-10-15 13:10:14 +0200

    Initial commit

--- src/DemoCard/Demo.tsx
+++ src/DemoCard/Demo.tsx
@@ -1,9 +1,12 @@
-import styles from './Demo.module.css';
+import { DumpTruck } from "../DumpTruck";
 
 export function Demo() {
   return (
     <section className={styles.card}>
       <span>buhu :'(</span>
+      <h1>
+      </h1>
+      <DumpTruck />
     </section>
   );
 }
